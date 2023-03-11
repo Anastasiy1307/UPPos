aa6d3d91db2061d36c00d458bce37ec01cdfa1a1
БарашенковИТ <@208-07>
2023-03-11 14:23:12 +0300

    рег

--- UPPos/Registration.xaml.cs
+++ UPPos/Registration.xaml.cs
@@ -28,7 +28,46 @@ namespace UPPos
         }
         private void Regis_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            frame1.Navigate(new LogIn(frame1));
+            string log = login.Text;
+            string pas = password.Text;
+            string pas1 = password_Copy.Text;
+            if (log != "")
+            {
+                if (pas != "")
+                {
+                    if (pas == pas1)
+                    {
+                        List<UP.Users> user = new List<UP.Users>() { new Users() };
+                        List<History> h = new List<History>() { new History() };
+                        int count = Entities1.GetContex().Users.Count();
+                        int count_h = Entities1.GetContex().History.Count();
+                        user[0].id = count + 1;
+                        user[0].login = log;
+                        user[0].password = pas1;
+                        h[0].id = count_h + 1;
+                        h[0].login = log;
+                        h[0].ip = Dns.GetHostName();
+                        h[0].dataZ = DateTime.Now;
+                        h[0].Block = DateTime.Now.AddMinutes(-30);
+                        Entities1.GetContex().History.Add(h[0]);
+                        Entities1.GetContex().Users.Add(user[0]);
+                        Entities1.GetContex().SaveChanges();
+                        frame1.Navigate(new LogIn(frame1));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пароли не совпадают");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите пароль");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин");
+            }
         }
         private void Back_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
