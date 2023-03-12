cb804edef1162164952623036cdcd619ff228ad4
Anastasiy1307 <100847962+Anastasiy1307@users.noreply.github.com>
2023-03-12 21:53:05 +0300

    Странички

--- UPPos/Glavnaya.xaml.cs
+++ UPPos/Glavnaya.xaml.cs
@@ -78,18 +78,18 @@ namespace UPPos
                     //Image.Visibility = Visibility.Hidden;
                 }
             }
-            //Вывод таблицы Сервис
-            List_Service = Entities1.GetContex().Service.ToList();
-            int countS = Entities1.GetContex().Service.Count();
-            sp.CountPageFlower = 3;
-            sp.CountlistFlower = countS;
-            LViewServ.ItemsSource = List_Service.Skip(0).Take(sp.CountPagesFlower).ToList();
-            //Вывод таблицы 
-            List_Result = Entities1.GetContex().Results.ToList();
-            int countR = Entities1.GetContex().Results.Count();
-            sp.CountPageFlower = 3;
-            sp.CountlistFlower = countR;
-            LViewResult.ItemsSource = List_Service.Skip(0).Take(sp.CountPagesFlower).ToList();
+            ////Вывод таблицы Сервис
+            //List_Service = Entities1.GetContex().Service.ToList();
+            //int countS = Entities1.GetContex().Service.Count();
+            //sp.CountPageFlower = 3;
+            //sp.CountlistFlower = countS;
+            //LViewServ.ItemsSource = List_Service.Skip(0).Take(sp.CountPagesFlower).ToList();
+            ////Вывод таблицы 
+            //List_Result = Entities1.GetContex().Results.ToList();
+            //int countR = Entities1.GetContex().Results.Count();
+            //sp.CountPageFlower = 3;
+            //sp.CountlistFlower = countR;
+            //LViewResult.ItemsSource = List_Result.Skip(0).Take(sp.CountPagesFlower).ToList();
         }
 
         public int TickCounter
@@ -97,6 +97,14 @@ namespace UPPos
             get { return (int)GetValue(TickCounterProperty); }
             set { SetValue(TickCounterProperty, value); }
         }
+
+        private void Glavnaya_Rezultat(object sender, MouseButtonEventArgs e)
+        {
+           
+        }
+
+       
+
 
         public int soxr = 0;
         private void Timer_Tick(object sender, EventArgs e)
@@ -147,7 +155,34 @@ namespace UPPos
             Result.Visibility = Visibility.Visible;
             LViewServ.Visibility = Visibility.Hidden;
             LViewResult.Visibility = Visibility.Visible;
+         
 
+            var all = Entities1.GetContex().Service.ToList();
+
+            ComboType.ItemsSource = all;
+            LViewServ.ItemsSource = all;
+
+            LViewServ.Visibility = Visibility.Visible;
+            LViewResult.Visibility = Visibility.Hidden;
+            Str.Visibility = Visibility.Visible;
+            TBoxSearch.Text = "";
+            List_Service = Entities1.GetContex().Service.ToList();
+            LViewServ.ItemsSource = Entities1.GetContex().Service.ToList();
+
+            sp.CountPageFlower = Entities1.GetContex().Service.ToList().Count;
+            DataContext = sp;
+
+            try
+            {
+                sp.CountPageFlower = Convert.ToInt32(kolvo_zapice); // если в текстовом поле есnь значение, присваиваем его свойству объекта, которое хранит количество записей на странице
+            }
+            catch
+            {
+                sp.CountPageFlower = List_Service.Count; // если в текстовом поле значения нет, присваиваем свойству объекта, которое хранит количество записей на странице количество элементов в списке
+            }
+            sp.CountlistFlower = List_Service.Count;  // присваиваем новое значение свойству, которое в объекте отвечает за общее количество записей
+            LViewServ.ItemsSource = List_Service.Skip(0).Take(sp.CountPageFlower).ToList();  // отображаем первые записи в том количестве, которое равно CountPage
+            sp.CurrentPage = 1; // текущая страница - это страница 1
         }
 
         private void Result_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -156,6 +191,62 @@ namespace UPPos
             Service.Visibility = Visibility.Visible;
             LViewResult.Visibility = Visibility.Hidden;
             LViewServ.Visibility = Visibility.Visible;
+
+            List<Results> result = new List<Results>();
+            List<Users> use = new List<Users>();
+            TBoxSearch.Text = "";
+            result = Entities1.GetContex().Results.ToList();
+            use = Entities1.GetContex().Users.ToList();
+            int counts1 = Entities1.GetContex().Results.Count();
+            //for (int i = 0; i < counts1; i++)
+            //{
+            //    if (result[i].login != user)
+            //    {
+            //        result.RemoveAt(i);
+            //        i--;
+            //        counts1--;
+            //    }
+            //}
+            LViewResult.ItemsSource = result;
+            LViewServ.Visibility = Visibility.Hidden;
+            LViewResult.Visibility = Visibility.Visible;
+        }
+        private void Glavnaya_GoPage(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock tb = (TextBlock)sender;
+
+            switch (tb.Uid)  // определяем, куда конкретно было сделано нажатие
+            {
+                case "prev":
+                    sp.CurrentPage--;
+                    break;
+                case "next":
+                    sp.CurrentPage++;
+                    break;
+                case "prev1":
+                    sp.CurrentPage = 1;
+                    break;
+                case "next1":
+                    {
+                        List<Service> fl = Entities1.GetContex().Service.ToList();
+                        int a = fl.Count;
+                        int b = Convert.ToInt32(kolvo_zapice);
+
+                        if (a % b == 0)
+                        {
+                            sp.CurrentPage = a / b;
+                        }
+                        else
+                        {
+                            sp.CurrentPage = a / b + 1;
+                        }
+                    }
+                    break;
+                default:
+                    sp.CurrentPage = Convert.ToInt32(tb.Text);
+                    break;
+            }
+            LViewServ.ItemsSource = List_Service.Skip(sp.CurrentPage * sp.CountPageFlower - sp.CountPageFlower).Take(sp.CountPageFlower).ToList();
         }
     }
 }
